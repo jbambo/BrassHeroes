b8d05a745f67750940efac2259b680211a379c51
jbambo <74155354+jbambo@users.noreply.github.com>
2021-02-22 08:55:22 +0100

    FightClub

--- app/src/main/java/com/example/brassheroes/MainActivity.java
+++ app/src/main/java/com/example/brassheroes/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.brassheroes;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    //TODO make logo png in the mipmap folder 200x200 px i think
+    //TODO title design (png or styled text or combined logo+title as png?)
+    //TODO design other screens
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set the immersive mode(complete fullscreen)
         setContentView(R.layout.activity_main);
+        int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
         //this is a test
     }
 }
